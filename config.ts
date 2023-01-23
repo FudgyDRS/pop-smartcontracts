4bdbe6c8e6886da2a1616a47574457a5ffafd2bb
FudgyDRS <yetimonsters@gmail.com>
2023-01-23 15:56:43 -0800

    reinitialize

---
+++ config.ts
@@ -0,0 +1,18 @@
+export default {
+  ERC721: {
+    Name: {
+      mainnet: "PoP NFT",
+      testnet: "PoP NFT",
+    },
+    Symbol: {
+      mainnet: "POP",
+      testnet: "tPOP",
+    },
+    Supply: {
+      Total: {
+        mainnet: "100",
+        testnet: "100",
+      },
+    },
+  },
+};
---
+++ hardhat.config.ts
@@ -0,0 +1,51 @@
+import type { HardhatUserConfig, NetworkUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-truffle5";
+import "hardhat-abi-exporter";
+import "hardhat-contract-sizer";
+import "solidity-coverage";
+import "dotenv/config";
+
+const bscTestnet: NetworkUserConfig = {
+  url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+  chainId: 97,
+  accounts: [process.env.KEY_TESTNET!],
+};
+
+const sepoliaTestnet: NetworkUserConfig = {
+  url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+  chainId: 11155111,
+  accounts: [process.env.KEY_TESTNET!],
+};
+
+const config = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    testnet1: bscTestnet,
+    testnet2: sepoliaTestnet,
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  abiExporter: {
+    path: "./data/abi",
+    clear: true,
+    flat: false,
+  },
+};
+
+export default config;
